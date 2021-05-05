b547604a0e238a0005bd00af9e342e4de31c7a8d
Abderrahmane <abdrr97@gmail.com>
2021-05-05 23:05:47 +0000

    fixing list project

--- list/main.js
+++ list/main.js
@@ -1,13 +1,20 @@
-const circle = document.querySelector('#circle');
+const btnGroup = document.querySelector('#btn-group')
+const contianer = document.querySelector('#contianer')
+const list = document.querySelector('#list')
+const grid = document.querySelector('#grid')
 
-circle.addEventListener('mouseenter', () => {
-    if (!circle.classList.contains('hover')) {
-        circle.classList.add('hover');
-    }
-});
 
-circle.addEventListener('mouseleave', () => {
-    if (circle.classList.contains('hover')) {
-        circle.classList.remove('hover');
+btnGroup.addEventListener('click', function (e) {
+    const target = e.target
+
+    if (target.id === 'list' || target.parentElement.id === 'list') {
+        list.classList.toggle('btn-dark')
+        grid.classList.toggle('btn-dark')
     }
-});
+    if (target.id === 'grid' || target.parentElement.id === 'grid') {
+        grid.classList.toggle('btn-dark')
+        list.classList.toggle('btn-dark')
+    }
+
+    contianer.classList.toggle('grid')
+})
