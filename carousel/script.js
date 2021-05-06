d6c8cdb3c08a996d3de80b0125315d4e73f96e6f
Abderrahmane <abdrr97@gmail.com>
2021-05-06 15:21:04 +0000

    adding carousel

---
+++ carousel/script.js
@@ -0,0 +1,44 @@
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
+
+const left = document.querySelector('#left')
+const right = document.querySelector('#right')
+const pause = document.querySelector('#pause')
+const play = document.querySelector('#play')
+
+const imageContainer = document.querySelector('.image-container')
+
+let index = 0
+
+
+const moveLeft = function () {
+
+    index = index < (images.length - 1) ? index += 1 : 0
+    let image = images[index]
+    imageContainer.style.backgroundImage = `url(./images/${image})`
+}
+
+const moveRight = function () {
+    if (index <= images.length) {
+        if (index === 0) {
+            index = images.length
+            return
+        }
+        index--
+    }
+    let image = images[index]
+    console.log(image)
+    imageContainer.style.backgroundImage = `url(./images/${image})`
+}
+
+left.addEventListener('click', moveLeft)
+index = images.length
+right.addEventListener('click', moveRight)
+
+let moving
+
+play.addEventListener('click', function () {
+    moving = setInterval(moveLeft, 1000)
+})
+pause.addEventListener('click', function () {
+    clearInterval(moving)
+})
