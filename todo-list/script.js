4a86e0808e3b1bc001768506259ccda6f5b4b408
Abderrahmane <abdrr97@gmail.com>
2021-05-10 12:33:30 +0000

    adding filter, clear all,fixing some bugs

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,9 +1,4 @@
-console.log('Todo List')
-
-// todo (input)
-// btn (button)
-// todos (list)
-
+console.log('todos')
 
 // todo input
 const todo = document.querySelector('#todo')
@@ -14,22 +9,22 @@ const addTodoBtn = document.querySelector('#btn')
 // todos list
 const todosList = document.querySelector('#todos')
 
-// todos array of todo object
+// clear all todos button
+const clearAll = document.querySelector('#clearAll')
 
+// filter
+const filter = document.querySelector('#filter')
+
+// todos array of todo object
 let todos = []
 
 
-
-// on add todo click
 const onAddTodoClick = function (e) {
     e.preventDefault()
 
     if (todo.value !== '') {
 
-        // -------------------
-
         let obj = {
-            id: new Date().getTime(),
             todo: todo.value,
             completed: false
         }
@@ -37,14 +32,25 @@ const onAddTodoClick = function (e) {
 
         localStorage.setItem('todos', JSON.stringify(todos))
 
-        // -------------------
-
-        createTodoListElement(todo.value)
+        createTodoListElement(todo)
     }
 }
 
 const onDeleteTodoClick = function (e) {
-
+    if (e.target.classList.contains('todo')) {
+        todos.forEach(function (todoElement, index) {
+            if (todoElement.todo === e.target.innerText) {
+                todoElement = {
+                    todo: todoElement.todo,
+                    completed: todoElement.completed ? false : true
+                }
+                todos[index] = todoElement
+                e.target.classList.toggle('completed')
+                localStorage.setItem('todos', JSON.stringify(todos))
+            }
+        })
+        console.log(todos);
+    }
     if (e.target.parentElement.classList.contains('delete-todo')) {
 
         todos.forEach(function (todoElement, index) {
@@ -65,11 +71,8 @@ const loadTodos = function (e) {
     } else {
         todos = JSON.parse(localStorage.getItem('todos'))
     }
-
-    // todos = JSON.parse(localStorage.getItem('todos'))
-
     todos.forEach(function (todoElement) {
-        createTodoListElement(todoElement.todo)
+        createTodoListElement(todoElement)
     });
 
 }
@@ -80,8 +83,11 @@ const createTodoListElement = function (value) {
     const a = document.createElement('a')
     const icon = document.createElement('i')
 
-    li.classList.add('todo')
-    li.innerText = value
+    li.classList.add('todo', 'slidedown')
+    li.innerText = value.value || value.todo
+    if (value.completed) {
+        li.classList.add('completed')
+    }
     a.classList.add('delete-todo')
     a.setAttribute('href', '#')
     icon.className = 'fas fa-trash'
@@ -91,10 +97,49 @@ const createTodoListElement = function (value) {
 
     todosList.appendChild(li)
     todo.value = ''
+
+
+    setTimeout(function () {
+        li.classList.add('show')
+    }, 100);
 }
 
+const onClearAllClick = function (e) {
+    todo.value = ''
+    todosList.innerHTML = ''
+    localStorage.clear()
+}
+
+// this loads all todos from localStorage and show them 
 document.addEventListener('DOMContentLoaded', loadTodos)
 
+// add todo
 addTodoBtn.addEventListener('click', onAddTodoClick)
 
+// deleting or completing a todo
 todosList.addEventListener('click', onDeleteTodoClick)
+
+// clearing all todos form LS
+clearAll.addEventListener('click', onClearAllClick)
+
+// filtring todos
+filter.addEventListener('input', (e) => {
+
+    let keyword = e.target.value.toLowerCase()
+
+    let filterdTodos = todos.filter(function (val) {
+        if (keyword != '' && val.todo.includes(keyword)) {
+            return val
+        }
+    })
+    todosList.innerHTML = ''
+    filterdTodos.forEach(function (todoElement) {
+        createTodoListElement(todoElement.todo)
+    });
+    if (keyword === '') {
+        filterdTodos = []
+        todos.forEach(function (todoElement) {
+            createTodoListElement(todoElement.todo)
+        });
+    }
+})
