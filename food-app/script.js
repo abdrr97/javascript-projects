b8f3aa2a6a3356ab1353395f4d70c54368513b44
Abderrahmane <abdrr97@gmail.com>
2021-05-22 12:42:10 +0100

    adding movie app and random food image app to jabvascript api projects

---
+++ food-app/script.js
@@ -0,0 +1,20 @@
+const imageContainer = document.querySelector('.image-container')
+const generateBtn = document.querySelector('#generate')
+let API_URL = 'https://foodish-api.herokuapp.com/api/'
+
+generateBtn.addEventListener('click', getImage)
+
+getImage()
+
+function getImage() {
+    generateBtn.classList.add('is-loading')
+
+    fetch(API_URL)
+        .then(function (res) {
+            return res.json()
+        }).then(function (data) {
+            let picture = data.image
+            imageContainer.style.backgroundImage = `url(${picture})`
+            generateBtn.classList.remove('is-loading')
+        })
+}
