72433f521bc55bd4a62f9072c0d02c547957c6dc
Abderrahmane <abdrr97@gmail.com>
2021-08-12 15:27:04 +0100

    fix some issues

--- dark-mode/script.js
+++ dark-mode/script.js
@@ -1,25 +1,17 @@
 const body = document.body
 const darkModeToggle = document.querySelector('#dark-mode-toggle')
 
-
 // load dark mode from local storage
 let isDarkMode = localStorage.getItem('dark_mode')
 if (isDarkMode === 'on') {
-    body.classList.add('dark-mode')
-    console.log(isDarkMode)
+  body.classList.add('dark-mode')
+  console.log(isDarkMode)
 }
 
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode')
 
-darkModeToggle.addEventListener('click', function () {
-    body.classList.toggle('dark-mode')
+  let toggle = body.classList.contains('dark-mode') ? 'on' : 'off'
 
-    let toggle = body.classList.contains('dark-mode') ? 'on' : 'off'
-
-    localStorage.setItem('dark_mode', toggle)
-
-    // if (body.classList.contains('dark-mode')) {
-    //     localStorage.setItem('dark_mode', 'on')
-    // } else {
-    //     localStorage.setItem('dark_mode', 'off')
-    // }
+  localStorage.setItem('dark_mode', toggle)
 })
