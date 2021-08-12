d0bbf937e69b9a30ae754624217d7d7032ec97c9
Abderrahmane <abdrr97@gmail.com>
2021-08-12 15:07:16 +0100

    circle project fix css

--- circle/app.js
+++ circle/app.js
@@ -2,10 +2,10 @@
 const hover = document.querySelector('#hover')
 
 const onMouseOver = function (e) {
-    hover.style.transform = 'scale(1.5)'
+  hover.style.transform = 'scale(1.5)'
 }
 const onMouseOut = function (e) {
-    hover.style.transform = 'scale(1)'
+  hover.style.transform = 'scale(1)'
 }
 
 hover.addEventListener('mouseover', onMouseOver)
