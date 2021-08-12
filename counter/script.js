097417b865b2139e0345c0662442acf76a06c378
Abderrahmane <abdrr97@gmail.com>
2021-08-12 15:11:00 +0100

    counter project fix css

--- counter/script.js
+++ counter/script.js
@@ -5,36 +5,35 @@ const increase = document.querySelector('#increase')
 let value = 0
 
 decrease.addEventListener('click', function () {
-    value--
-    text.innerText = value
+  value--
+  text.innerText = value
 
-    text.style.color = value < 0 ? 'red' : value === 0 ? 'blue' : 'green'
-    // if (value < 0) {
-    //     text.style.color = 'red'
-    // } else if (value === 0) {
-    //     text.style.color = 'blue'
-    // }
-    console.log('decrease')
+  text.style.color = value < 0 ? 'red' : value === 0 ? 'blue' : 'green'
+  // if (value < 0) {
+  //     text.style.color = 'red'
+  // } else if (value === 0) {
+  //     text.style.color = 'blue'
+  // }
+  console.log('decrease')
 })
 reset.addEventListener('click', function () {
-    value = 0
-    text.innerText = value
+  value = 0
+  text.innerText = value
 
-    if (value === 0)
-        text.style.color = 'blue'
+  if (value === 0) text.style.color = 'blue'
 
-    console.log('reset')
+  console.log('reset')
 })
 increase.addEventListener('click', function () {
-    value++
-    text.innerText = value
+  value++
+  text.innerText = value
 
-    text.style.color = value > 0 ? 'green' : value === 0 ? 'blue' : 'red'
+  text.style.color = value > 0 ? 'green' : value === 0 ? 'blue' : 'red'
 
-    // if (value > 0) {
-    //     text.style.color = 'green'
-    // } else if (value === 0) {
-    //     text.style.color = 'blue'
-    // }
-    console.log('increase')
+  // if (value > 0) {
+  //     text.style.color = 'green'
+  // } else if (value === 0) {
+  //     text.style.color = 'blue'
+  // }
+  console.log('increase')
 })
--- faq/scripts.js
+++ faq/scripts.js
@@ -1,27 +1,25 @@
 const faqs = [
-    {
-        title: "Q 1?",
-        content: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque dolore incidunt, repellendus odio vitae quasi exceptu`
-    },
-    {
-        title: "Q 2?",
-        content: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque dolore incidunt, repellendus odio  ullam. Dolorum illo soluta at ipsum?`
-    },
-    {
-        title: "Q 3?",
-        content: `Lorem ipsum dolor sit, amet vitae quasi excepturi eum voluptate vero ullam. Dolorum illo soluta at ipsum?`
-    },
-    {
-        title: "Q 4?",
-        content: `Lorem ipsum dol olorum illo soluta at ipsum?`
-    }
+  {
+    title: 'Q 1?',
+    content: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque dolore incidunt, repellendus odio vitae quasi exceptu`,
+  },
+  {
+    title: 'Q 2?',
+    content: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque dolore incidunt, repellendus odio  ullam. Dolorum illo soluta at ipsum?`,
+  },
+  {
+    title: 'Q 3?',
+    content: `Lorem ipsum dolor sit, amet vitae quasi excepturi eum voluptate vero ullam. Dolorum illo soluta at ipsum?`,
+  },
+  {
+    title: 'Q 4?',
+    content: `Lorem ipsum dol olorum illo soluta at ipsum?`,
+  },
 ]
-
 
 const cardList = document.querySelector('#card-list')
 faqs.forEach(function (faq) {
-
-    cardList.innerHTML += `
+  cardList.innerHTML += `
         <div class="card">
             <div class="card-header">
                 <h3>${faq.title}</h3>
@@ -39,24 +37,24 @@ faqs.forEach(function (faq) {
 const btnsToggle = document.querySelectorAll('.btn-toggle')
 
 btnsToggle.forEach(function (btnToggle) {
-
-    btnToggle.addEventListener('click', function () {
-        btnsToggle.forEach(function (btnToggle) {
-            btnToggle.innerHTML = '<i class="fa fa-chevron-down"></i>'
-            btnToggle.parentElement.parentElement.classList.remove('expanded')
-        })
-        btnToggle.parentElement.parentElement.classList.add('expanded')
-
-        let isExpanded = btnToggle.parentElement.parentElement.classList.contains('expanded')
-
-        // if (isExpanded) {
-        //     btnToggle.innerHTML = '<i class="fa fa-times-circle"></i>'
-        // } else {
-        //     btnToggle.innerHTML = '<i class="fa fa-chevron-down"></i>'
-        // }
-
-        btnToggle.innerHTML = isExpanded
-            ? '<i class="fa fa-times-circle"></i>'
-            : '<i class="fa fa-chevron-down"></i>'
+  btnToggle.addEventListener('click', function () {
+    btnsToggle.forEach(function (btnToggle) {
+      btnToggle.innerHTML = '<i class="fa fa-chevron-down"></i>'
+      btnToggle.parentElement.parentElement.classList.remove('expanded')
     })
+    btnToggle.parentElement.parentElement.classList.add('expanded')
+
+    let isExpanded =
+      btnToggle.parentElement.parentElement.classList.contains('expanded')
+
+    // if (isExpanded) {
+    //     btnToggle.innerHTML = '<i class="fa fa-times-circle"></i>'
+    // } else {
+    //     btnToggle.innerHTML = '<i class="fa fa-chevron-down"></i>'
+    // }
+
+    btnToggle.innerHTML = isExpanded
+      ? '<i class="fa fa-times-circle"></i>'
+      : '<i class="fa fa-chevron-down"></i>'
+  })
 })
