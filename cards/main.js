8b041331c728b86235b8d1ce1a0be1f724e19cb3
Abderrahmane <abdrr97@gmail.com>
2021-08-17 21:00:12 +0100

    fixing some issues

--- cards/main.js
+++ cards/main.js
@@ -1,59 +1,42 @@
-const cardsContainerEl = document.querySelector('#cards-container')
 const colors = ['green', 'red', 'blue', 'black', 'yellow']
-const startGameBtn = document.querySelector('#btn')
-const cardsListEl = document.querySelectorAll('.card')
-let GAME_TIMER = 11
-let gameTimerInterval
-const gameReset = (e) => {
-  clearInterval(gameTimerInterval)
-  GAME_TIMER = 10
-  cardsListEl.forEach((card, idx) => {
-    card.style.backgroundColor = `rgba(204, 204, 204, 0.1${idx})`
-  })
-}
+const btn = document.querySelector('#btn')
+const generatedCardsListEl = document.querySelectorAll('.card.random')
+const cardsListEl = document.querySelectorAll('.card.result')
 
-gameReset()
+let patternList = [],
+  init = null,
+  diffucltyCount = 16
 
-startGameBtn.addEventListener('click', function (e) {
-  startGameBtn.textContent = `Gaming ...`
-  e.target.disabled = true
+btn.addEventListener('click', (e) => {
+  init = setInterval(() => {
+    // setTimeout(() => (generatedCardsListEl[i].style.background = 'white'), 200)
 
-  gameTimerInterval = setInterval(() => {
-    console.log(GAME_TIMER)
-    GAME_TIMER--
-    startGameBtn.textContent = `Timer: 00:${GAME_TIMER}:00`
-    e.target.style.color = GAME_TIMER % 2 === 0 ? 'red' : 'white'
-    if (GAME_TIMER === -1) {
-      // time is up
-      alert(`time is up`)
-      startGameBtn.textContent = `Restart`
-      gameReset(e, true)
-      console.log('time is up')
-    }
-  }, 1000)
-
-  //   card list
-  cardsListEl.forEach(function (card, idx) {
-    card.style.backgroundColor = 'white'
-    card.addEventListener('click', function (e) {
-      e.target.style.backgroundColor =
-        colors[Math.floor(Math.random() * colors.length)]
-      if (check()) {
-        alert('You Won')
+    let i = Math.floor(Math.random() * generatedCardsListEl.length)
+    let rc = Math.floor(Math.random() * colors.length)
+    if (patternList.length !== diffucltyCount) {
+      while (patternList.find((p) => p.i === i)) {
+        i = Math.floor(Math.random() * generatedCardsListEl.length)
       }
-    })
-  })
+    }
+    generatedCardsListEl[i].style.background = colors[rc]
+    patternList.push({ i, rc })
+
+    e.target.textContent = `Generating ... | ${patternList.length}`
+    if (patternList.length === diffucltyCount) {
+      e.target.textContent = `Done !`
+      clearInterval(init)
+      patternList = []
+      // generatedCardsListEl.forEach((card) => {
+      //   card.style.background = 'white'
+      // })
+    }
+  }, 100)
 })
 
-const check = function () {
-  let firstElColor
-  let count = 0
-  firstElColor = cardsListEl[0].style.backgroundColor
+cardsListEl.forEach((card, idx) => {
+  card.addEventListener('click', (e) => {
+    const i = Math.floor(Math.random() * colors.length)
 
-  cardsListEl.forEach(function (card, idx) {
-    if (firstElColor == card.style.backgroundColor) {
-      count++
-    }
+    e.target.style.background = colors[i]
   })
-  return count === cardsListEl.length
-}
+})
